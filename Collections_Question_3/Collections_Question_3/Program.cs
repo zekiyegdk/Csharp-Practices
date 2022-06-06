6f21a1f9ed57d4897a4d07357469bd27d85a2514
Zekiye Gedik <zkyegdk@outlook.com>
2022-06-06 20:22:50 +0300

    Collections question 3 added

---
+++ Collections_Question_3/Collections_Question_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+/* Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız. */
+
+namespace Collections_Question_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            Console.WriteLine("Lütfen cümle yazınız.");
+            string sentences = Console.ReadLine().ToLower();
+            char[] letters = {'a','e','ı','i','o','ö','u','ü' };
+
+            foreach (char sentence in sentences)
+            {
+                foreach (char letter in letters)
+                {
+                    if (sentence == letter)
+                        
+                        arrayList.Add(sentence);                    
+                }
+            }
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine("Dizinin elemanları: " + item);
+            }
+            
+        }
+    }
+}
